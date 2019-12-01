7b7167246975b963f6dc8db3ca43b01d8d3f0e13
Synpy <37023198+Synpy@users.noreply.github.com>
2019-11-30 16:08:05 -0800

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -4,40 +4,33 @@ import sys #In order to terminate the program
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
-#Fill in start
-serverName = 'hostname'
-serverPort = 12345
+
+serverPort = 12346 #port used
 serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
-#Fill in end
+serverSocket.listen(1) #limit # of connections to 1
+
 while True :
     #Establish the connection
     print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
     try :
-        message = connectionSocket.recv(2048).decode()
+        message = connectionSocket.recv(1024).decode()
         filename = message.split()[1]
         f = open(filename[1:])
         outputdata = f.read()
         #Send one HTTP header line into socket
-        #Fill in start
-        connectionSocket.send('HTTP/1.1 200 OK\nContent-Type: text/html\n\n'.encode())
-        #connectionSocket.send(outputdata.encode())
-        #Fill in end
+        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
         connectionSocket.send("\r\n".encode())
+
         connectionSocket.close()
     except IOError:
-        print("err")
         #Send response message for file not found
-        #Fill in start
         errorMessage = ('404 Not Found: ' + filename + "\r\n")
         connectionSocket.send(errorMessage.encode())
         connectionSocket.close()
-        #Fill in end
         #Close client socket
-        #connectionSocket.close()
 serverSocket.close()
 sys.exit()#Terminate the program after sending the corresponding data
